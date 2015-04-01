eea32556e0883b5b5e6d4c2bdd794022d504de0a
verona1024 <vladfilin94@gmail.com>
2015-04-02 00:07:39 +0300

    О нас

--- app/src/main/java/verona1024/cleverhouse/dialogs/AboutUsDialog.java
+++ app/src/main/java/verona1024/cleverhouse/dialogs/AboutUsDialog.java
@@ -5,6 +5,9 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import verona1024.cleverhouse.activityes.R;
 
 /**
  * Created by verona1024 on 01.04.15.
@@ -14,18 +17,18 @@ public class AboutUsDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Блаблабла")
-                .setPositiveButton("ОК", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // FIRE ZE MISSILES!
-                    }
-                })
-                .setNegativeButton("Не ок", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
-                    }
-                });
-        // Create the AlertDialog object and return it
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_about_us, null))
+        .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                AboutUsDialog.this.getDialog().dismiss();
+            }
+        });
         return builder.create();
     }
 }
